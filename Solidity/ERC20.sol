feca18d8e5fbcfb2ccc4d5f53f9cbf1c1f072241
cob-byte <jebarcelona2020@plm.edu.ph>
2022-11-13 11:36:59 +0800

    solidity

---
+++ Solidity/ERC20.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Ticket is ERC20, Ownable {
+    mapping(address => uint256) private _balances;
+
+    constructor() ERC20("Ticket", "TKT") {
+        _mint(msg.sender, 1000000 * 10 ** decimals());
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function balanceOf(address account) public view virtual override returns (uint256) {
+        return _balances[account];
+    }
+
+    function transfer(address to, uint256 amount) public virtual override returns (bool) {
+        address owner = _msgSender();
+        _transfer(owner, to, amount);
+        return true;
+    }
+
+}
